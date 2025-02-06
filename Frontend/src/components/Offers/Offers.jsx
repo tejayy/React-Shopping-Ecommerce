5f97e00b0816804b5153c78d2b15b774ee337ecb
Tejas Jaybhaye <117623822+tejayy@users.noreply.github.com>
2025-02-06 22:45:37 +0530

    Added offers css

--- Frontend/src/components/Offers/Offers.jsx
+++ Frontend/src/components/Offers/Offers.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import "./Offers.css";
 import exclusive_image from "../Assets/exclusive_image.png";
 
 const Offers = () => {
   return (
-    <div className="offer">
+    <div className="offers">
       <div className="offers-left">
         <h1>Exclusive</h1>
         <h1>offers for you</h1>
