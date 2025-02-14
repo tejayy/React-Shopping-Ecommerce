e2fde357a6c0a111ebe5461864661f42c5cf7509
tejayy <tbj456@gmail.com>
2025-02-14 19:34:16 +0530

    shop banner for category

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Shop from "./pages/Shop";
 import Product from "./pages/Product";
 import Cart from "./pages/Cart";
 import Login from "./pages/LoginSignup";
 import ShopCatagory from "./pages/ShopCatagory";
 import LoginSignup from "./pages/LoginSignup";
+import men_banner from "./components/assets/banner_mens.png";
+import women_banner from "./components/assets/banner_women.png";
+import kids_banner from "./components/assets/banner_kids.png";
 
 function App() {
   return (
@@ -15,9 +18,18 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCatagory catagory="mens" />} />
-          <Route path="/womens" element={<ShopCatagory catagory="womens" />} />
-          <Route path="/kids" element={<ShopCatagory catagory="kids " />} />
+          <Route
+            path="/mens"
+            element={<ShopCatagory banner={men_banner} catagory="mens" />}
+          />
+          <Route
+            path="/womens"
+            element={<ShopCatagory banner={women_banner} catagory="womens" />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCatagory banner={kids_banner} catagory="kids " />}
+          />
           <Route path="product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
           </Route>
--- Frontend/src/pages/Shop.jsx
+++ Frontend/src/pages/Shop.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Hero from "../components/Hero/Hero";
 import Popular from "../components/Popular/Popular";
 import Offers from "../components/Offers/Offers";
