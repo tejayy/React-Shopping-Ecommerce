73e5e933397f055d420528286a8be811cb16bdc4
tejayy <tbj456@gmail.com>
2025-02-15 17:50:25 +0530

    added Cart logic

--- Frontend/src/Context/ShopContext.jsx
+++ Frontend/src/Context/ShopContext.jsx
@@ -1,10 +1,36 @@
-import {createContext} from "react";
+/* eslint-disable react/prop-types */
+import {createContext, useState} from "react";
 import all_products from "../components/assets/all_product";
 
 export const ShopContext = createContext(null);
 
+const getDefaultCart = () => {
+  let cart = {};
+
+  for (let index = 0; index < all_products.length + 1; index++) {
+    cart[index] = 0;
+  }
+  return cart;
+};
+
 const ShopContextProvider = (props) => {
-  const contextValue = {all_product: all_products};
+  const [cartItems, setCartItems] = useState(getDefaultCart());
+
+  const addToCart = (itemId) => {
+    console.log(cartItems);
+    setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}));
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1}));
+  };
+
+  const contextValue = {
+    all_product: all_products,
+    cartItems,
+    addToCart,
+    removeFromCart,
+  };
 
   return (
     <ShopContext.Provider value={contextValue}>
