de4c66f9e9a50bb90500b3e8244533b954aedcc4
tejayy <tbj456@gmail.com>
2025-02-15 16:38:14 +0530

    added Breadcrums for product

---
+++ Frontend/src/components/Breadcrums/Breadcrum.jsx
@@ -0,0 +1,16 @@
+import "./Breadcrum.css";
+import breadcrum_arrow from "../assets/breadcrum_arrow.png";
+
+const Breadcrum = (props) => {
+  const {product} = props;
+
+  return (
+    <div className="breadcrum">
+      HOME <img src={breadcrum_arrow} alt="" /> SHOP{" "}
+      <img src={breadcrum_arrow} alt="" /> {product.category}{" "}
+      <img src={breadcrum_arrow} alt="" /> {product.name}
+    </div>
+  );
+};
+
+export default Breadcrum;
--- Frontend/src/pages/Product.jsx
+++ Frontend/src/pages/Product.jsx
@@ -1,5 +1,18 @@
+import {useContext} from "react";
+import {useParams} from "react-router-dom";
+import {ShopContext} from "../Context/ShopContext";
+import Breadcrum from "../components/Breadcrums/Breadcrum";
+
 const Product = () => {
-  return <div>Product</div>;
+  const {all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
+
+  return (
+    <div>
+      <Breadcrum product={product} />
+    </div>
+  );
 };
 
 export default Product;
